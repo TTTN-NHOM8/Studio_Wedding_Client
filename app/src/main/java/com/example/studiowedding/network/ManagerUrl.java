8a83d55ea9f397e3314761803a82e14604bd258a
Đạt <datlowashere@gmail.com>
2023-12-07 23:02:59 +0700

    Dat Update 7/12/2023

--- app/src/main/java/com/example/studiowedding/network/ManagerUrl.java
+++ app/src/main/java/com/example/studiowedding/network/ManagerUrl.java
@@ -4,7 +4,7 @@ public class ManagerUrl {
 
 
 
-    public static final String BASE_URL = "http://192.168.1.173:3000/api/";
+    public static final String BASE_URL = "http://192.168.1.4:3000/api/";
 
     //account
     public static final String ACCOUNT = "account/Login";
